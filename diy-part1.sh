26e65afff85fc32cd452eafe5818056806a5767e
jdj0603 <jdj0603@163.com>
2021-04-02 20:48:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-cd openwrt/package
+cd openwrt
 git clone https://github.com/kenzok8/openwrt-packages.git
 git clone https://github.com/kenzok8/small.git
 
